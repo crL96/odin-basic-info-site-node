9f0f1c17ff4d1a25a6ab7ab605993e58c5cba1a6
crL96 <190506813+crL96@users.noreply.github.com>
2025-04-29 08:40:32 +0200

    Add server 404 error handling

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ import fs from "node:fs";
 
 const server = http.createServer((req, res) => {
     
-    const filename = "./index.html"
+    const filename = "./inex.html"
     
     fs.readFile(filename, (error, data) => {
-        res.writeHead(200, { "Content-Type": "text/html"});
+        if (error) {
+            res.writeHead(404, { "Content-Type": "text/html" })
+            return res.end("404 Not Found")
+        }
+
+        res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
-        res.end()
+        return res.end()
     });
 
 }).listen(8080);
